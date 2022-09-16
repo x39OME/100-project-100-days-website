9a9915c2f4e7acb7b40fc540cdc519f2ad857470
Sam <74021826+x39OME@users.noreply.github.com>
2022-09-16 22:58:37 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,36 +6,35 @@ function menuToggle(){
 	navigation.classList.toggle('active');
 };
 
-// Countdown
+// Countdown Done !!
+// const body = document.body;
+// const endTime = new Date('9 16 2022 23:59:59');
+// const daysEl = document.getElementById('days');
+// const hoursEl = document.getElementById('hours');
+// const minutesEl = document.getElementById('minutes');
+// const secondsEl = document.getElementById('seconds');
 
-const body = document.body;
-const endTime = new Date('9 16 2022 23:59:59');
-const daysEl = document.getElementById('days');
-const hoursEl = document.getElementById('hours');
-const minutesEl = document.getElementById('minutes');
-const secondsEl = document.getElementById('seconds');
+// setInterval(updateCountdown, 1000)
 
-setInterval(updateCountdown, 1000)
-
-function updateCountdown() {
-	const startTime = new Date();
-	const diff = endTime - startTime;
-	const days = Math.floor(diff / 1000 / 60 / 60 / 24);
-	const hours = Math.floor(diff / 1000 / 60 / 60) % 24;
-	const minutes = Math.floor(diff / 1000 / 60) % 60;
-	const seconds = Math.floor(diff / 1000) % 60;
-	daysEl.innerHTML = days;
-	hoursEl.innerHTML = hours < 10 ? '0'+hours : hours;
-	minutesEl.innerHTML = minutes < 10 ? '0'+minutes : minutes;
-	secondsEl.innerHTML = seconds < 10 ? '0'+seconds : seconds;
-};
+// function updateCountdown() {
+// 	const startTime = new Date();
+// 	const diff = endTime - startTime;
+// 	const days = Math.floor(diff / 1000 / 60 / 60 / 24);
+// 	const hours = Math.floor(diff / 1000 / 60 / 60) % 24;
+// 	const minutes = Math.floor(diff / 1000 / 60) % 60;
+// 	const seconds = Math.floor(diff / 1000) % 60;
+// 	daysEl.innerHTML = days;
+// 	hoursEl.innerHTML = hours < 10 ? '0'+hours : hours;
+// 	minutesEl.innerHTML = minutes < 10 ? '0'+minutes : minutes;
+// 	secondsEl.innerHTML = seconds < 10 ? '0'+seconds : seconds;
+// };
 
 // Time & Date (Footer)
 setInterval(function(){
 document.getElementById('date').innerHTML = Date();
 },1000);
 
-// scroll To Top
+// Scroll To Top
 let scrollTop = document.querySelector('.scrollTop');
 
 window.onscroll = function (){
